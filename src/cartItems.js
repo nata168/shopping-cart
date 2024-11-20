23a643c0a2c4e42281ead1fb3010395993a63cc5
nata168 <zeroflugel@gmail.com>
2024-11-20 17:40:54 +0700

    Clean cart items

--- src/cartItems.js
+++ src/cartItems.js
@@ -1,11 +1,5 @@
 export { cartItems };
 
 const cartItems = [
-  {
-    name: "Omniscient Reader's Viewpoint Volume 1",
-    amount: 1,
-    priceTotal: 11.99,
-  },
-]
 
-window.cartItems = cartItems;
+]
